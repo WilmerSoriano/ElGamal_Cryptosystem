3c8edcb22a7ded8d427e3b4b331d9fd2bfed71cd
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-11-01 01:49:53 -0500

    encryption made including mod and extended algorithm

--- elgamal.py
+++ elgamal.py
@@ -17,7 +17,10 @@
               by only using public key (Y), p, and r.
             - This is possible because p is a large prime number and with also both private key being random and large
             the possibilities starts to become very large and time consuming to attack by brute force.
+
+        NOTE for demonstration purpose the values will be hardcoded
 """
+
 
 """
     Public over the channel:
@@ -25,24 +28,59 @@
         r = A primative root mod (p)
 """
 
-#1. Key-generation:
-    #1. Select a private-key(X) as the following: X < p-1
+def ext_euclidean(a, b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        gcd, x1, y1 = ext_euclidean(b, a % b)
 
+        x = y1
+        y = x1 - (a // b) * y1
+
+        return gcd, x, y
+    
+
+def pow_mod(r, exp, p):
+    result = 1
+    p = r % p
+    while exp > 0:
+        if exp % 2 == 1:
+            result = (result * r) % p
+        exp = exp // 2
+        r = (r * r) % p
+    return result
+
+#1. Key-generation:
+def gen_keys(p, r):
+    #1. Select a private-key(X) as the following: X < p-1
+    private_key = 84
+    if p-1 <= private_key:
+        raise Exception("Value for private-key does not meet X < p-1")
+    
     #2. Compute for Y = r^X mod(p)
+    public_key = pow_mod(r, private_key, p)
+    
+    return public_key, private_key
 
 #2. Encryption:
+def encrypt(public_key, M, p, r):
     #1. Some message(M) must be M < p
+    if p < M:
+        raise Exception("Length of message is longer then p")
 
     #2. At random select private-key(k) as the following: k < p-1
-
+    k = 50
+    if p-1 <= 50:
+        raise Exception("Length of message is longer then p")
     #3. Compute the following:
         # K = Y^k mod(p)
-
+    K = pow_mod(public_key, k, p)  
         # C1 = r^k mod(p)
-         
+    C1 = pow_mod(r, k, p)
         # C2 = K*M mod(p)
-
+    C2 = (K*M) % p
     #4. Return (C1, C2)
+    return C1, C2
 
 #3. Decryption:
     #1. Receive (C1,C2)
@@ -55,4 +93,17 @@
 
 
 if __name__ == '__main_':
+    p = 563
+    r = 5
+
+    public_key, private_key = gen_keys(p, r)
+    print("Generating public-key and private-key...")
+    print("="*30)
+
+    M = 100
+    ciphertext = encrypt(public_key, M, p, r)
+    
+    print("Encrypting secret message!")
+    print("="*30)
+
 
